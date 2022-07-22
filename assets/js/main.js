c6831afc5b3c4206b4e78866bd8eaf957f327e86
mengjie-liu <mengjie.liu@yale.edu>
2022-07-22 14:28:23 -0400

    ha

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,100 @@
 $(document).ready(function () {
+  // $(window).resize(function () {
+  //   var height = $(window).height();
+  //   var width = $(window).width();
+
+  //   if (width > height) {
+  //     // Landscape
+  //   } else {
+  //     console.log("ll");
+  //     $(".canv").css("transform", "rotate(90deg)");
+  //     // Portrait
+  //     // $("#mode").text("PORTRAIT");
+  //   }
+  // });
+
+  // $(function() {
+  var width = $(window).width();
+  var height = $(window).height();
+  var alphaRate, betaRate, gammaRate;
+  var alpha, beta, gamma;
+
+  function requestDeviceMotion() {
+    if (
+      typeof window.DeviceMotionEvent !== "undefined" &&
+      typeof window.DeviceMotionEvent.requestPermission === "function"
+    ) {
+      window.DeviceMotionEvent.requestPermission()
+        .then((permissionState) => {
+          if (permissionState === "granted") {
+            window.addEventListener("devicemotion", (e) => {
+              alphaRate = e.rotationRate.alpha;
+              betaRate = e.rotationRate.beta;
+              // gammaRate = e.rotationRate.gamma;
+              console.log(alphaRate + ", " + betaRate);
+              // if (Math.round(Math.abs(alphaRate)) >= 10) {
+              //   console.log(betaRate);
+              //   if (alphaRate >= 1.5) {
+              //     console.log(alphaRate);
+              //   } else if (alphaRate <= -1.5) {
+              //     // console.log(alphaRate);
+              //     $(".image").ripples(
+              //       "drop",
+              //       (width * 1) / 5 + ((width * 3) / 5) * Math.random(),
+              //       height + 170,
+              //       200,
+              //       0.0015 * aradious
+              //     );
+              //   }
+              // }
+
+              // if (Math.round(Math.abs(gammaRate)) >= 10) {
+              //   // console.log(betaRate);
+              //   let gradious = scale(
+              //     Math.round(Math.abs(gammaRate)),
+              //     0,
+              //     200,
+              //     0,
+              //     100
+              //   );
+              //   if (gammaRate >= 1.5) {
+              //     console.log(gammaRate);
+              //     $(".image").ripples(
+              //       "drop",
+              //       -190,
+              //       (height * 1) / 5 + ((height * 3) / 5) * Math.random(),
+              //       200,
+              //       0.00025 * gradious
+              //     );
+              //   } else if (gammaRate <= -1.5) {
+              //     // console.log(alphaRate);
+              //     $(".image").ripples(
+              //       "drop",
+              //       width + 190,
+              //       (height * 1) / 5 + ((height * 3) / 5) * Math.random(),
+              //       200,
+              //       0.00025 * gradious
+              //     );
+              //   }
+              // }
+            });
+          }
+        })
+        .catch(console.error);
+    } else {
+      console.log("not permitted");
+    }
+  }
+
+  function scale(num, in_min, in_max, out_min, out_max) {
+    return ((num - in_min) * (out_max - out_min)) / (in_max - in_min) + out_min;
+  }
+
+  $(document).click(function () {
+    requestDeviceMotion();
+  });
+  // });
+
   var movementStrength = 950;
   var height = movementStrength / $(window).height();
   var width = movementStrength / $(window).width();
